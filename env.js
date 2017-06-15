d1d3ce09b3b9fdc16d13a908a37cbfb9c9e71719
xijing <298511448@qq.com>
2017-06-15 11:37:03 +0800

    1

--- env.js
+++ env.js
@@ -1,5 +1,6 @@
 let ENV = {}
 
+// log environment
 console.log('env is:' + process.env.NODE_ENV )
 if (process.env.NODE_ENV === "development"){
     ENV.MANGODB_CONN_STR = 'mongodb://10.211.55.5:27017/vue-webpack'
