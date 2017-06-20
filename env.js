14825164e5c70fbff1ffd48e6a96ea9a9ad2710a
xijing <298511448@qq.com>
2017-06-20 17:35:15 +0800

    1

--- env.js
+++ env.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === "development"){
     ENV.REDIS_CONN_PORT = 6379
 } else {
     ENV.MANGODB_CONN_STR = 'mongodb://127.0.0.1:27017/'
-    ENV.MANGOOSE_CONN_URL = '127.0.0.1'
+    ENV.MANGOOSE_CONN_URL = '116.62.143.122'
     ENV.REDIS_CONN_URL = '127.0.0.1'
     ENV.REDIS_CONN_PORT = 6379
 }
