33da2344bb6bb3d8783c8f88e85e77c2fff40d3c
xijing <298511448@qq.com>
2017-06-30 17:24:28 +0800

    1

--- src/pages/Schedule.vue
+++ src/pages/Schedule.vue
@@ -32,14 +32,17 @@
 		},
 		methods: {
 			tapDay(year,month,date){
-				date = '今' ? this.ymd = "今天" : this.ymd = year + '年' + month + '月' + date + '日'
-                this.popupVisible = true
+				date == '今' ? this.ymd = "今天" : this.ymd = year + '年' + month + '月' + date + '日'
+				this.schedule = [{content:'当前日期没有记录'}];
 				let url = '/api/schedule'
                 this.$http.get(url,{params:{year:year,month:month,date:date}}).then((response) => {
-                    this.$data.schedule = response.data.schedule
+					if (response.data.schedule.length > 0){
+						this.$data.schedule = response.data.schedule
+					}
                 }, (response) => {
                     console.log('oops, data is error')
                 })
+				this.popupVisible = true
             }
 		}
 	}
@@ -47,8 +50,10 @@
 <style scoped lang="scss">
 	.list-box {
 		width:100vw;padding:20px;box-sizing:border-box;max-height:50vh;overflow:auto;
-		.list {
-			padding:10px 20px;border:#ccc solid 1px;border-radius:3px;
+		.list {padding:10px 0;
+			li {
+				padding:10px 20px;border:#ccc solid 1px;border-radius:3px;margin-bottom:10px;
+			}
 		}
 	}
 </style>
