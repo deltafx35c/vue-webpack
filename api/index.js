bf2bada92882b6af6df4617842145b652cf91e21
xijing <xijing@rongzi.com>
2017-11-29 15:12:50 +0800

    1

--- api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-
-router.use(require('./users'))
-router.use(require('./photos'))
-router.use(require('./resources'))
-router.use(require('./schedule'))
-
-module.exports = router
